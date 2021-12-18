6dc3308ff1e90f64b172bbac1bfa86f16f7088fb
dannyb94 <danielle.ussery@bryanuniversity.edu>
2021-12-17 19:15:57 -0800

    week4

--- week3/join-split/join-split.py
+++ week3/join-split/join-split.py
@@ -1,3 +1,14 @@
-csv = 'Eric,John,Michael,Terry,Graham:TerryG;Brian'
-friends_list = ["John", "Jacob", "Jingleheimer", "Schmidt"]
+csv = 'Eric,John,Michael,Terry,Graham,TerryG,Brian'
+friends_list = []
+
+s = csv.split(",")
+
+friends_list.append(s[0])
+friends_list.append(s[1])
+friends_list.append(s[2])
+friends_list.append(s[3])
+friends_list.append(s[4])
+friends_list.append(s[5])
+friends_list.append(s[6])
+
 print(friends_list)
---
+++ week4/guessing-game.py
@@ -0,0 +1,28 @@
+import random
+
+# Generate a random number between 1 and 9 (including 1 and 9). Ask the user to guess the number, then tell them whether they guessed too low, too high, or exactly right. 
+# Keep the game going until the user types “exit”
+# Keep track of how many guesses the user has taken, and when the game ends, print this out.
+
+num = random.randint(1, 9)
+guess = 0
+track = 0
+
+while guess != num and guess != "exit":
+    guess = input("Guess a number between 1 & 9. If you wish to end the game, please type 'exit'. Enter your number: ")
+
+    if guess == "exit":
+        print("Giving up already! Come back and try again soon.")
+        break
+
+    guess = int(guess)
+    track +- 1
+    if guess not in range (1, 9):
+        print("Doh! Please input a number between 1 & 9.")
+    elif guess < num:
+        print("Too low bub.")
+    elif guess > num:
+        print("Shootin' too high there pal.")
+    else:
+        if track == 1:
+            print("You got it dude! :thumbs_up:")
