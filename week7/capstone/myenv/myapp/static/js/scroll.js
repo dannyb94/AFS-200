52b86186dac07befcba5da11b21daee138abd23c
dannyb94 <danielle.ussery@bryanuniversity.edu>
2022-01-17 13:47:23 -0800

    week7

--- week7/capstone/myenv/myapp/static/js/scroll.js
+++ week7/capstone/myenv/myapp/static/js/scroll.js
@@ -1,4 +1,15 @@
-// document.getElementsByClassName('sections').prototype.forEach(anchor => { //'a[href^ = "#"]'
+[...document.getElementsByClassName('sections')].forEach(anchor => { //'a[href^ = "#"]'
+    anchor.addEventListener("click", function(e){
+        console.log(anchor, "anchor")
+        e.preventDefault();
+        document.querySelector(this.getAttribute("href")).scrollIntoView({
+            behavior: "smooth"
+        });
+    });
+});
+console.log("yo")
+// nav = document.getElementsByClassName('sections')
+// for (var i = 0; i < nav.length; i++)(anchor => { //'a[href^ = "#"]'
 //     anchor.addEventListener("click", function(e){
 //         e.preventDefault();
 //         document.querySelector(this.getAttribute("href")).scrollIntoView({
@@ -6,13 +17,3 @@
 //         });
 //     });
 // });
-
-nav = document.getElementsByClassName('sections')
-for (var i = 0; i < nav.length; i++)(anchor => { //'a[href^ = "#"]'
-    anchor.addEventListener("click", function(e){
-        e.preventDefault();
-        document.querySelector(this.getAttribute("href")).scrollIntoView({
-            behavior: "smooth"
-        });
-    });
-});
